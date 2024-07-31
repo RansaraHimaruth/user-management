d0d76487c771a4fc95b89de4fa903b11381fa4f7
RansaraHimaruth <ransara.20221894@iit.ac.lk>
2024-07-31 15:24:29 +0530

    fix

--- user-management/app/api/topics/route.ts
+++ user-management/app/api/topics/route.ts
@@ -37,7 +37,7 @@ export async function GET(request: any) {
             return NextResponse.json({ message: "User not authenticated" }, { status: 401 });
         }
         await connectDB();
-        const topics = await Topic.find({ clerkId: userId }).populate("creator");
+        const topics = await Topic.findById({ clerkId: userId }).populate("creator");
         return NextResponse.json({topics}, {status: 200});
     } catch (error) {
         return NextResponse.json({message: "Failed to fetch topics"}, {status: 500});
