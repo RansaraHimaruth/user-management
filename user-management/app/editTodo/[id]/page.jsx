38a15a695d7dce1dc8db4ed7fa9e3e12c0691661
RansaraHimaruth <ransara.20221894@iit.ac.lk>
2024-07-31 20:38:02 +0530

    fix

--- user-management/app/editTodo/[id]/page.jsx
+++ user-management/app/editTodo/[id]/page.jsx
@@ -1,43 +1,4 @@
-// import EditTodo from '@/components/EditTodo'
-// import React from 'react'
-// import axios from 'axios';
-
-// async function EditTopic({params}) {
-  
-//   const {id} = params;
-//   console.log('id: ',id);
-
-//   const getTopicById = async (id) => {
-//     try {
-//       const response = await axios.get(`/api/topics/${id}` , {cache: "no-store"});
-
-//       if (response.status !== 200) {
-//         throw new Error('Failed to fetch topic');
-//       }
-
-//       return response.data;
-
-//     } catch (error) {
-//       console.log('Error fetching topic: ', error);
-//     }
-//   }
-
-//   const {topic} = await getTopicById(id);
-//   const {title, description} = topic;
-//   console.log('title: ',title);
-//   console.log('description: ',description);
-
-
-//   return (
-//     <div>
-//       <EditTodo id={id} title={title} description={description}/>
-//     </div>
-//   )
-// }
-
-// export default EditTopic
-
-import EditTodo from '@/components/EditTodo'
+import EditTopicForm from '@/components/EditTopicForm'
 import React from 'react'
 import axios from 'axios';
 
@@ -48,7 +9,7 @@ async function EditTopic({params}) {
 
   const getTopicById = async (id) => {
     try {
-      const response = await axios.get(`/api/topics/${id}`);
+      const response = await axios.get(`/api/topics/${id}` , {cache: "no-store"});
 
       if (response.status !== 200) {
         throw new Error('Failed to fetch topic');
@@ -58,24 +19,18 @@ async function EditTopic({params}) {
 
     } catch (error) {
       console.log('Error fetching topic: ', error);
-      
     }
   }
 
-  const topicData = await getTopicById(id);
+  const {topic} = await getTopicById(id);
+  const {title, description} = topic;
+  console.log('title: ',title);
+  console.log('description: ',description);
 
-  if (!topicData) {
-    return <div>Error loading topic{topicData}</div>; // Handle the error case
-  }
-
-  const { topic } = topicData;
-  const { title, description } = topic;
-  console.log('title: ', title);
-  console.log('description: ', description);
 
   return (
     <div>
-      <EditTodo id={id} title={title} description={description} />
+      <EditTopicForm id={id} title={title} description={description}/>
     </div>
   )
 }
