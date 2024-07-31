d4249c1a082198a623978acdca289a1bd543f67b
RansaraHimaruth <ransara.20221894@iit.ac.lk>
2024-07-31 23:10:30 +0530

    fix

--- user-management/app/api/topics/[id]/route.ts
+++ user-management/app/api/topics/[id]/route.ts
@@ -1,10 +1,18 @@
 import connectDB from "@/libs/database";
 import Topic from "@/models/topic";
 import { NextResponse } from "next/server";
+import { auth, currentUser } from "@clerk/nextjs/server";
+import User from "@/models/user.modal";
 
 export async function PUT(request: any, { params }: { params: any }) {
     try {
         const { title, description } = await request.json();
+        const { userId } = auth();
+        console.log(userId);
+        if (!userId) {
+            return NextResponse.json({ message: "User not authenticated" }, { status: 401 });
+        }
+        await connectDB();
         const topic = await Topic.findById(params.id);
         topic.title = title;
         topic.description = description;
@@ -19,6 +27,11 @@ export async function PUT(request: any, { params }: { params: any }) {
 
 export async function GET(request: any, { params }: { params: any }) {
     try {
+        const { userId } = auth();
+        console.log(userId);
+        if (!userId) {
+            return NextResponse.json({ message: "User not authenticated" }, { status: 401 });
+        }
         await connectDB();
   
       // Usage: Topic.findById(params.id)
